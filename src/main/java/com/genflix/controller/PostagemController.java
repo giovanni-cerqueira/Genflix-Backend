e971e0dce71c9973c5cff8ce2e0fe11f4a4763a9
Giovanni <80859834+Giovanni-SVC@users.noreply.github.com>
2021-11-15 20:49:07 -0300

    Alterando endpoint

--- src/main/java/com/genflix/controller/PostagemController.java
+++ src/main/java/com/genflix/controller/PostagemController.java
@@ -47,6 +47,11 @@ public class PostagemController {
 		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
 	}
 	
+	@GetMapping("/criticas")
+	public ResponseEntity<List<Postagem>> getAllCriticas(){
+		return ResponseEntity.ok(postagemRepository.criticas());
+	}
+	
 	@GetMapping("/criticas/{criticas}")
 	public ResponseEntity<List<Postagem>> getAllCriticas(@PathVariable String criticas){
 		return ResponseEntity.ok(postagemRepository.findAllByCriticasContainingIgnoreCase(criticas));
